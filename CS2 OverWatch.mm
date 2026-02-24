926a3256d534c8c5b8c45a01e680ba10d76e3dd1
corp14x3 <61080511+corp14x3@users.noreply.github.com>
2026-02-24 05:50:13 +0300

    Create CS2 OverWatch.mm

---
+++ CS2 OverWatch.mm
@@ -0,0 +1,356 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="CS2 OverWatch" FOLDED="false" ID="ID_107805750" CREATED="1771417652682" MODIFIED="1771421419632" BACKGROUND_COLOR="#0099ff" STYLE="narrow_hexagon" UNIFORM_SHAPE="true" MAX_WIDTH="55.83837 pt" MIN_WIDTH="55.83837 pt" VGAP_QUANTITY="12.75 pt">
+<font SIZE="10"/>
+<hook NAME="MapStyle" zoom="1.3310001">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" auto_compact_layout="false" show_note_icons="true" associatedTemplateLocation="template:/BigMap.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_rıght" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_1273250224" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="CENTER" MAX_WIDTH="90 pt" MIN_WIDTH="90 pt" VGAP_QUANTITY="2 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="LINEAR_PATH" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1273250224" STARTINCLINATION="100.5 pt;0 pt;" ENDINCLINATION="100.5 pt;6.75 pt;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="9" BOLD="true" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plaın/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plaın/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" TEXT_ALIGN="LEFT">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#33ff00" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_rıght" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1358928635">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#0000ff" TRANSPARENCY="255" DESTINATION="ID_1358928635"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_rıght" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120 pt" MIN_WIDTH="120 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      uygulama acilinca kucuk bir login ekrani cikicak asagisinda register yazicak
+    </p>
+    <p>
+      remember me tusu olsun
+    </p>
+    <p>
+      uygulama hem mobilde hem webde olacak o yuzden react kullan
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="backend" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_rıght" ID="ID_653467011" CREATED="1771421541351" MODIFIED="1771424304839" HGAP_QUANTITY="576.74998 pt" VSHIFT_QUANTITY="-105 pt" VGAP_QUANTITY="2 pt">
+<hook NAME="FreeNode"/>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      python ile yazilacak fastapi kullanilacak , banlananlarin sayisini hesaplamak banlarini hesaplamak icin steam api kullanabilirsin
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      (tum db baglanti bilgilerini, api keyleri vesaire tek bir .ini dosyasi icin ayarlarsan sevinirim)
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="database yapisi" ID="ID_851423489" CREATED="1771421586507" MODIFIED="1771421784050" HGAP_QUANTITY="-190.74999 pt" VSHIFT_QUANTITY="62.25 pt" VGAP_QUANTITY="2 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      postgresql kullanilacak
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="kullanici profilleri" ID="ID_551065841" CREATED="1771421631400" MODIFIED="1771423867162" HGAP_QUANTITY="-180.24999 pt" VSHIFT_QUANTITY="40.5 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      user_id, user_email ,user_photo, user_nick, user_password ,user_lvl
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      user_id: sistem tarafindan olusturulan kullaniciya direkt otomatik olarak atanacak
+    </p>
+    <p>
+      login : user_email ve user_password eslesmesi ile yapilacak (sifreler sifrelenmis bir sekilde db de tutulacak)
+    </p>
+    <p>
+      user_lvl: her bir kullanici banlandiginda bu bir articak (1xp artacak)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="reported" ID="ID_1038677581" CREATED="1771421977054" MODIFIED="1771422867944" HGAP_QUANTITY="-177.99999 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      reported_accs, account_bans , report_from
+    </p>
+    <p>
+      reported_accs: kullanicilarin raporladigi oyuncularin hesap linkleri(demostaki demo_accs linkleri buraya tek tek eklenecek ayni link varsa eklenmeyecek)
+    </p>
+    <p>
+      account_bans: bu ilk raporlandigi andan itibaren steam hesaplarindaki ban sayisi (bu artinca kisinin banlandigi ortaya cikacak)
+    </p>
+    <p>
+      report_from: bu kullanici user_id listesi tutacak boylece kullanicilar ban attiklarinda kime ban attiklarini anlayabilecekler (profilde banned from you kisminda bilgisi bu liste sayesinde guncellenicek)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="demos" ID="ID_956984843" CREATED="1771422331574" MODIFIED="1771423223869" HGAP_QUANTITY="-178.74999 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      demo_id,demo_file_path,demo_accs,uploaded_from,download_count,upload_timestamp
+    </p>
+    <p>
+      demo_id:sunucuya yuklenirken demolara rastgele bir id verilecek bunun sebebide ayni isimde bir demo olursa diye sunucudaki diger demo dosyasinin ustune yazmasin)<br/>
+    </p>
+    <p>
+      demo_file_path:sunucuda demonun tutuldugu yer
+    </p>
+    <p>
+      demo_accs: demoyu yuklerken asagi eklenen profil linkleri
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="ranks" ID="ID_1019448770" CREATED="1771423785149" MODIFIED="1771423951232" HGAP_QUANTITY="-178.74999 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      rank_image,rank_name,required_xp
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      egerki kullanini xp si rankin requiredinden yuksekse o rankli almis olur
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="leaderboard" ID="ID_1984809458" CREATED="1771423975378" MODIFIED="1771424196698" HGAP_QUANTITY="-177.99999 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      buradada clientte gosterilecek olan leader boardin kendisi mevcut(burada hafta icerisinde,ay icerisinde ve yil icerisinde en cok ban yedirtene bakacagiz (profildeki banned from you sayisina gore siralama yapilacak))
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="api yapisi" ID="ID_1536963329" CREATED="1771421607449" MODIFIED="1771424337182" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="-29.25 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kisilerin idileri uzerinden yuruyecek bir api sistemi olacak (gerekeni yaparsin)<br/>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="settings" POSITION="bottom_or_rıght" ID="ID_1221471273" CREATED="1771417756444" MODIFIED="1771418341213" HGAP_QUANTITY="61.25 pt" VSHIFT_QUANTITY="-17.25 pt" VGAP_QUANTITY="2 pt">
+<node TEXT="game replays path" ID="ID_769488298" CREATED="1771417772170" MODIFIED="1771419338260" HGAP_QUANTITY="38 pt" VSHIFT_QUANTITY="-4.5 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      path yazip kaydedicez appdataya kayit olacak
+    </p>
+    <p>
+      bir duzenle tusu olacak duzenle modunu acinca ayni tus kaydet tusuna donecek
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="log out" ID="ID_784324233" CREATED="1771417790762" MODIFIED="1771419283884" HGAP_QUANTITY="38.75 pt" VSHIFT_QUANTITY="-3.75 pt"/>
+<node TEXT="banned from you" ID="ID_1060854973" CREATED="1771419235056" MODIFIED="1771424142417" HGAP_QUANTITY="44 pt" VSHIFT_QUANTITY="-5.25 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      burada kullanicinin kendi raporladigi kisiler egerki ban yerse buraya banlattiklari kisilerin steam profillerinin linkleri eklenecek
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="user profile" ID="ID_1182667876" CREATED="1771420342844" MODIFIED="1771420386202" HGAP_QUANTITY="43.25 pt" VSHIFT_QUANTITY="-47.25 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kullanicilar kendi nicknamelerini profil fotolarini ve ranklarini buradan goruntuleyebilecekler
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="demos" POSITION="top_or_left" ID="ID_1328156921" CREATED="1771417819388" MODIFIED="1771421906113" HGAP_QUANTITY="-205.74999 pt" VSHIFT_QUANTITY="0.75 pt" VGAP_QUANTITY="2 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sayfanin icerisinde insanlarin uploadladigi demolari gorebilecegiz ve kac kisinin indirdigini de yan tarafinda gorecegiz
+    </p>
+    <p>
+      demolari sisteme yuklenme tarihi ve indirme sayisina gore siralayabilecegiz
+    </p>
+    <p>
+      demolarin sagindaki kucuk indir tusundan demoyu indirebilecegiz ayarlardan ayarladigimiz replays path klasorune
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="upload demo" ID="ID_1147000898" CREATED="1771417837002" MODIFIED="1771423134323" HGAP_QUANTITY="-226.74999 pt" VSHIFT_QUANTITY="-1.5 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      burada direk klasorun icerisinden demoyu secip demoyu uploadlayacagiz(demo sunucuya yuklenecek,sunucuda da demos adinda bir klasor olacak)
+    </p>
+    <p>
+      asagisindada suspect's steam diye bir yer olacak buralarada demodaki suphelilerin steamleri eklenecek
+    </p>
+    <p>
+      yani demoyu indiren kisiler oradan direk kullanicilari raporlayabilecekler
+    </p>
+    <p>
+      sadece demoyu indirdikten sonra kisiler raporlama yapabilecek
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="downloaded demos" POSITION="bottom_or_rıght" ID="ID_1824971700" CREATED="1771418303922" MODIFIED="1771418332946" HGAP_QUANTITY="57.5 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      burada indirilen demolari gorebilecegiz sag tarafindaki sil tusundan silebilecegiz
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="leader board" POSITION="top_or_left" ID="ID_950011533" CREATED="1771418353061" MODIFIED="1771422280538" HGAP_QUANTITY="-205.74999 pt" VSHIFT_QUANTITY="21.75 pt" VGAP_QUANTITY="2 pt"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      burada raporlanan kisilerin ban yemesini saglayan profillerin
+    </p>
+    <p>
+      haftalik aylik ve yillik siralamasi olacak
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ve uygulamada bir rank sistemi olacak
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      kullanicilarin raporladiklari kisi ban yerse bunun icin 1xp alacaklari bu sistemde
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      10-15-20-25-30-35-40-45-50-55-60-65
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      her 12 lvl in bir tanesi icin yukaridaki xp miktarlarini sirasiyla doldurmalari gerekecek
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      leaderboarda birine tikladigimizda o kisinin profilini goruntuledigimizde yine ayni sekilde onun bilgilerini gorecegiz banned from you, nicki ,rozetleri(rank atlayarak aldigi badgeler) vs.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</map>
